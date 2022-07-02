9769f7e55a6fcc8043676d354b542927365d8743
Li4e99 <li4e99@gmail.com>
2022-07-02 18:33:00 +0200

    Исправлено задание 2.1

--- Lesson1/Lesson1/BankAccount.cs
+++ Lesson1/Lesson1/BankAccount.cs
@@ -2,78 +2,38 @@
 {
     public class BankAccount
     {
-        private static int _accNumber = 0;
         private int _AccountNumber;
         private double _Balance;
         public enum Type { credit, deposit, current }
         private Type _Type;
-        /// <summary>
-        /// Метод для генерации уникального номера карты
-        /// </summary>
-        /// <returns></returns>
-        public int BankAcc()
+
+        public void SetAccountNumber (int accountNumber)
         {
-            _AccountNumber = _accNumber++;
+           _AccountNumber= accountNumber;
+        }
+        public int GetAccountNumber ()
+        {
             return _AccountNumber;
         }
-        public int AccountNumber
-        {
-            get => _AccountNumber;
-            set => _AccountNumber = value;
-        }
 
-        public double Balance
-        {
-            get => _Balance;
-            set => _Balance = value;
-        }
-
-        public Type AccountType
-        {
-            get { return _Type; }
-            set => _Type = value;
-        }
-        /// <summary>
-        /// Конструктор дефолтный
-        /// </summary>
-        public BankAccount()
-        {
-            _AccountNumber = 0;
-            _Balance = 0;
-            _Type = Type.current;
-            BankAcc();
-        }
-        /// <summary>
-        /// Коструктор для заполнения поля баланс
-        /// </summary>
-        /// <param name="balance"> Баланс счета</param>
-        public BankAccount( double balance)
+        public void SetBalance(int balance)
         {
             _Balance = balance;
-            _Type = Type.current;
-            BankAcc();
         }
-        /// <summary>
-        /// конструктор для заполнения поля тип банковского счета
-        /// </summary>
-        /// <param name="type">Тип счета</param>
-        public BankAccount(Type type)
+        public double GetBalance()
         {
-            _Type = type;
-            _Balance = default;
-            BankAcc();
-        }
-        /// <summary>
-        /// конструктор для заполнения баланса и типа банковского счета
-        /// </summary>
-        /// <param name="balance">Баланс счета</param>
-        /// <param name="type">Тип счета</param>
-        public BankAccount(double balance,Type type)
-        {
-            _Balance = balance;
-            _Type = type;
-            BankAcc();
+            return _Balance;
         }
 
+        public void SetType(BankAccount.Type type)
+        {
+            _Type = type;
+        }
+        public BankAccount.Type GetType()
+        {
+            return _Type;
+        }
+
+      
     }
 }
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,21 +1,12 @@
 ﻿using Lesson1;
 
 
-BankAccount bankAccount1 = new();
-Console.WriteLine($"Баланс {bankAccount1.Balance},номер счета {bankAccount1.AccountNumber}, тип счета {bankAccount1.AccountType}");
-
-BankAccount bankAccount2 = new(1005.21);
-Console.WriteLine($"Баланс {bankAccount2.Balance},номер счета {bankAccount2.AccountNumber}, тип счета {bankAccount2.AccountType}");
-
-BankAccount bankAccount3 = new(BankAccount.Type.deposit);
-Console.WriteLine($"Баланс {bankAccount3.Balance},номер счета {bankAccount3.AccountNumber}, тип счета {bankAccount3.AccountType}");
-
-BankAccount bankAccount4 = new(312.2323, BankAccount.Type.credit); //тут пример для задания 2.4
-Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
-bankAccount4.Balance = 100500;
-bankAccount4.AccountType = BankAccount.Type.deposit;
-bankAccount4.AccountNumber = 123;
-Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
+BankAccount bankAccount = new();
+bankAccount.SetBalance(100000000);
+bankAccount.SetType (BankAccount.Type.credit);
+bankAccount.SetAccountNumber (123);
+Console.WriteLine($"Номер счета {bankAccount.GetAccountNumber()}, баланс счета {bankAccount.GetBalance()}, тип счета {bankAccount.GetType()}");
+Console.ReadKey(true);
 
 Console.ReadKey(true);
 
