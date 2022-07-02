d3416fc16c0322efba9690fc5375c5b04326b721
Li4e99 <li4e99@gmail.com>
2022-07-02 18:02:50 +0200

    Задание 2.4 В классе все методы для заполнения и получения значений полей заменить на свойства. Написать тестовый пример.

--- Lesson1/Lesson1/BankAccount.cs
+++ Lesson1/Lesson1/BankAccount.cs
@@ -7,7 +7,10 @@
         private double _Balance;
         public enum Type { credit, deposit, current }
         private Type _Type;
-
+        /// <summary>
+        /// Метод для генерации уникального номера карты
+        /// </summary>
+        /// <returns></returns>
         public int BankAcc()
         {
             _AccountNumber = _accNumber++;
@@ -16,17 +19,23 @@
         public int AccountNumber
         {
             get => _AccountNumber;
+            set => _AccountNumber = value;
         }
 
         public double Balance
         {
             get => _Balance;
+            set => _Balance = value;
         }
 
         public Type AccountType
         {
             get { return _Type; }
+            set => _Type = value;
         }
+        /// <summary>
+        /// Конструктор дефолтный
+        /// </summary>
         public BankAccount()
         {
             _AccountNumber = 0;
@@ -34,20 +43,31 @@
             _Type = Type.current;
             BankAcc();
         }
-
+        /// <summary>
+        /// Коструктор для заполнения поля баланс
+        /// </summary>
+        /// <param name="balance"> Баланс счета</param>
         public BankAccount( double balance)
         {
             _Balance = balance;
             _Type = Type.current;
             BankAcc();
         }
-
+        /// <summary>
+        /// конструктор для заполнения поля тип банковского счета
+        /// </summary>
+        /// <param name="type">Тип счета</param>
         public BankAccount(Type type)
         {
             _Type = type;
             _Balance = default;
             BankAcc();
         }
+        /// <summary>
+        /// конструктор для заполнения баланса и типа банковского счета
+        /// </summary>
+        /// <param name="balance">Баланс счета</param>
+        /// <param name="type">Тип счета</param>
         public BankAccount(double balance,Type type)
         {
             _Balance = balance;
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -2,19 +2,19 @@
 
 
 BankAccount bankAccount1 = new();
-
 Console.WriteLine($"Баланс {bankAccount1.Balance},номер счета {bankAccount1.AccountNumber}, тип счета {bankAccount1.AccountType}");
 
 BankAccount bankAccount2 = new(1005.21);
-
 Console.WriteLine($"Баланс {bankAccount2.Balance},номер счета {bankAccount2.AccountNumber}, тип счета {bankAccount2.AccountType}");
 
 BankAccount bankAccount3 = new(BankAccount.Type.deposit);
-
 Console.WriteLine($"Баланс {bankAccount3.Balance},номер счета {bankAccount3.AccountNumber}, тип счета {bankAccount3.AccountType}");
 
-BankAccount bankAccount4 = new(312.2323, BankAccount.Type.credit);
-
+BankAccount bankAccount4 = new(312.2323, BankAccount.Type.credit); //тут пример для задания 2.4
+Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
+bankAccount4.Balance = 100500;
+bankAccount4.AccountType = BankAccount.Type.deposit;
+bankAccount4.AccountNumber = 123;
 Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
 
 Console.ReadKey(true);
