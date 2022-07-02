e8b602a000698f21579b3397f7dd6637ba241e5f
Li4e99 <li4e99@gmail.com>
2022-07-02 19:12:18 +0200

    Добавлено задание 2.5 (*)Добавить в класс счет в банке два метода: снять со счета и положить на счет. Метод снять со счета проверяет, возможно ли снять запрашиваемую сумму, и в случае положительного результата изменяет баланс.

--- Lesson1/Lesson1/BankAccount.cs
+++ Lesson1/Lesson1/BankAccount.cs
@@ -2,38 +2,116 @@
 {
     public class BankAccount
     {
+        private static int _accNumber = 0;
         private int _AccountNumber;
-        private double _Balance;
+        private decimal _Balance;
         public enum Type { credit, deposit, current }
         private Type _Type;
-
-        public void SetAccountNumber (int accountNumber)
+        /// <summary>
+        /// Метод для генерации уникального номера карты
+        /// </summary>
+        /// <returns></returns>
+        public int BankAcc()
         {
-           _AccountNumber= accountNumber;
-        }
-        public int GetAccountNumber ()
-        {
+            _AccountNumber = _accNumber++;
             return _AccountNumber;
         }
+        public int AccountNumber
+        {
+            get => _AccountNumber;
+            set => _AccountNumber = value;
+        }
 
-        public void SetBalance(int balance)
+        public decimal Balance
+        {
+            get => _Balance;
+            set => _Balance = value;
+        }
+
+        public Type AccountType
+        {
+            get { return _Type; }
+            set => _Type = value;
+        }
+        /// <summary>
+        /// Конструктор дефолтный
+        /// </summary>
+        public BankAccount()
+        {
+            _AccountNumber = 0;
+            _Balance = 0;
+            _Type = Type.current;
+            BankAcc();
+        }
+        /// <summary>
+        /// Коструктор для заполнения поля баланс
+        /// </summary>
+        /// <param name="balance"> Баланс счета</param>
+        public BankAccount(decimal balance)
         {
             _Balance = balance;
+            _Type = Type.current;
+            BankAcc();
         }
-        public double GetBalance()
+        /// <summary>
+        /// конструктор для заполнения поля тип банковского счета
+        /// </summary>
+        /// <param name="type">Тип счета</param>
+        public BankAccount(Type type)
         {
+            _Type = type;
+            _Balance = default;
+            BankAcc();
+        }
+        /// <summary>
+        /// конструктор для заполнения баланса и типа банковского счета
+        /// </summary>
+        /// <param name="balance">Баланс счета</param>
+        /// <param name="type">Тип счета</param>
+        public BankAccount(decimal balance, Type type)
+        {
+            _Balance = balance;
+            _Type = type;
+            BankAcc();
+        }
+        /// <summary>
+        /// Метод для внесения денег
+        /// </summary>
+        /// <param name="cash">Сумма для внесения</param>
+        /// <returns></returns>
+        public decimal AddMoney(decimal cash)
+        {
+            _Balance = _Balance + cash;
+            Console.WriteLine($"На Ваш счет зачислено {cash} рублей");
+            return _Balance;
+        }
+        /// <summary>
+        /// Метод для снятия денег
+        /// </summary>
+        /// <param name="cash">Сумма для снятия</param>
+        /// <returns></returns>
+        public decimal TakeMoney(decimal cash)
+        {
+            if (_Type != BankAccount.Type.credit)
+            {
+                if (cash <= _Balance)
+                {
+                    _Balance = _Balance - cash;
+                    Console.WriteLine($"Остаток на Вашем балансе - {_Balance}");
+                    return _Balance;
+                }
+                else
+                    Console.WriteLine("Недостаточно средств.");
+                return _Balance;
+            }
+            else
+                _Balance = _Balance - cash;
+                Console.WriteLine($"Остаток на Вашем балансе - {_Balance}");
             return _Balance;
         }
 
-        public void SetType(BankAccount.Type type)
-        {
-            _Type = type;
-        }
-        public BankAccount.Type GetType()
-        {
-            return _Type;
-        }
-
-      
     }
+
+
 }
+
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using Lesson1;
 
+BankAccount bankAccount4 = new(100500, BankAccount.Type.credit); //тут пример для задания 2.5 с кредитным счетом
+Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
+bankAccount4.TakeMoney(100500);
+Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
+bankAccount4.AddMoney(1123004);
+Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
+bankAccount4.TakeMoney(1123005);
 
-BankAccount bankAccount = new();
-bankAccount.SetBalance(100000000);
-bankAccount.SetType (BankAccount.Type.credit);
-bankAccount.SetAccountNumber (123);
-Console.WriteLine($"Номер счета {bankAccount.GetAccountNumber()}, баланс счета {bankAccount.GetBalance()}, тип счета {bankAccount.GetType()}");
-Console.ReadKey(true);
+BankAccount bankAccount5 = new(100500, BankAccount.Type.deposit); //тут пример для задания 2.5 с депощитным счетом
+Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
+bankAccount5.TakeMoney(100500);
+Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
+bankAccount5.AddMoney(1123004);
+Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
+bankAccount5.TakeMoney(1123005);
+
 
 Console.ReadKey(true);
 
