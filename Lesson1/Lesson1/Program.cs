94251c1425ac4346c8a1c396016c8e312c9cf1e7
Li4e99 <li4e99@gmail.com>
2022-07-03 22:11:01 +0200

    Задание 3.1 В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой. У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,21 +1,15 @@
 ﻿using Lesson1;
 
+//3.1
 BankAccount bankAccount4 = new(100500, BankAccount.Type.credit); //тут пример для задания 2.5 с кредитным счетом
 Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
-bankAccount4.TakeMoney(100500);
-Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
-bankAccount4.AddMoney(1123004);
-Console.WriteLine($"Баланс {bankAccount4.Balance},номер счета {bankAccount4.AccountNumber}, тип счета {bankAccount4.AccountType}");
-bankAccount4.TakeMoney(1123005);
 
 BankAccount bankAccount5 = new(100500, BankAccount.Type.deposit); //тут пример для задания 2.5 с депощитным счетом
 Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
-bankAccount5.TakeMoney(100500);
+
+bankAccount5.MoveCash(bankAccount4, 500);
 Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
-bankAccount5.AddMoney(1123004);
-Console.WriteLine($"Баланс {bankAccount5.Balance},номер счета {bankAccount5.AccountNumber}, тип счета {bankAccount5.AccountType}");
-bankAccount5.TakeMoney(1123005);
 
 
-Console.ReadKey(true);
+
 
